33c2ed70c6a1c6ddd7150d3da447fb6af660746f
Felipe Lalanne <flalanne@niclabs.cl>
2013-10-02 16:50:05 -0300

    Added Traffic monitor for mobile data traffic

---
+++ src/cl/niclabs/adkmobile/monitor/Traffic.java
@@ -0,0 +1,42 @@
+package cl.niclabs.adkmobile.monitor;
+
+import cl.niclabs.adkmobile.monitor.data.DataFields;
+import cl.niclabs.adkmobile.monitor.data.DataObject;
+import cl.niclabs.adkmobile.monitor.listeners.MonitorListener;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Administrador.
+ *         Created 25-09-2013.
+ */
+public class Traffic extends Monitor {
+
+	@Override
+	public DataFields getDataFields(int eventType) {
+		// TODO Auto-generated method stub.
+		return null;
+	}
+
+	@Override
+	protected void onActivateEvent(int eventType) {
+		// TODO Auto-generated method stub.
+		
+	}
+
+	@Override
+	protected void onDataReceived(MonitorListener listener, int eventType,
+			DataObject data) {
+		// TODO Auto-generated method stub.
+		
+	}
+
+	@Override
+	protected void onDeactivateEvent(int eventType) {
+		// TODO Auto-generated method stub.
+		
+	}
+	
+
+}
