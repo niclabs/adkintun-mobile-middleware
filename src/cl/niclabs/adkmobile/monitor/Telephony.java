48332920a848dfaa79348447a17c83a76185c953
Felipe Lalanne <flalanne@niclabs.cl>
2013-10-18 14:48:55 -0300

    Added missing network types. Only create list of neighbours if there is enough data available.

--- src/cl/niclabs/adkmobile/monitor/Telephony.java
+++ src/cl/niclabs/adkmobile/monitor/Telephony.java
@@ -3,7 +3,10 @@ package cl.niclabs.adkmobile.monitor;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -28,6 +31,10 @@ import cl.niclabs.adkmobile.monitor.listeners.TelephonyListener;
 /**
  * TODO Put here a description of what this class does.
  * 
+ * Requires permissions
+ * - android.permission.ACCESS_COARSE_LOCATION
+ * - android.permission.READ_PHONE_STATE
+ * 
  * @author Mauricio Castro. Created 04-10-2013.
  */
 public class Telephony extends AbstractMonitor {
@@ -35,6 +42,19 @@ public class Telephony extends AbstractMonitor {
 	public class ServiceBinder extends Binder {
 		public Telephony getService() {
 			return Telephony.this;
+		}
+	}
+	
+	public enum TelephonyStandard {
+		GSM(1), CDMA(2);
+		
+		int value;
+		private TelephonyStandard(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
 		}
 	}
 
@@ -55,30 +75,26 @@ public class Telephony extends AbstractMonitor {
 		public static String TELEPHONY_GSM_CID = "gsm_cid";
 		public static String TELEPHONY_GSM_LAC = "gsm_lac";
 		public static String TELEPHONY_GSM_PSC = "gsm_psc";
-		public static String TELEPHONY_NEIGHBOR_GMS_PSC = "neighbor_gsm_psc";
-		public static String TELEPHONY_NEIGHBOR_GSM_CID = "neighbor_gsm_cid";
-		public static String TELEPHONY_NEIGHBOR_GSM_LAC = "neighbor_gsm_lac";
 		
-		public static String TELEPHONY_NEIGHBOR_SIGNAL_STRENGTH = "neighbor_signal_strength";
 		public static String TELEPHONY_SIGNAL_BER = "signal_ber";
 		public static String TELEPHONY_SIGNAL_STRENGTH = "signal_strength";
 		public static String TELEPHONY_STANDARD = "telephony_std";
 
-		public static String TOWER_TYPE = "tower_type";
 		public static String TELEPHONY_NETWORK_TYPE = "network_type";
-		public static String TELEPHONY_OPERATOR_NAME = "operator_name";
-		// TODO: TELEPHONY_STANDARD and TOWER_TYPE are not used, same thing for CDMA_ECIO, EVDO_ECIO
+		public static String TELEPHONY_OPERATOR_MCC = "operator_mcc";
+		public static String TELEPHONY_OPERATOR_MNC = "operator_mnc";
 	}
 	
 	/**
 	 * Network types as defined in android.telephony.TelephonyManager
 	 *
-	 * @author Administrador.
+	 * @author Mauricio Castro <mauricio@niclabs.cl>.
 	 *         Created 17-10-2013.
 	 */
-	public static enum NetworkType{
-		UMTS(1), HSDPA(2),EDGE(3),CDMA(4),GPRS(5),LTE(6),HSPA(7),HSPAP(8
-				), OTHER(9);
+	public static enum NetworkType {
+		RTT(1), CDMA(2), EDGE(3), EHRPD(4), EVDO_0(5), EVDO_A(6), EVDO_B(7), GPRS(
+				8), HSDPA(9), HSPA(10), HSPAP(11), HSUPA(12), IDEN(13), LTE(14), UMTS(
+				15), UNKNOWN(16), OTHER(0);
 		
 		/**
 		 * Get the network type from the TelephonyManager constants
@@ -87,22 +103,38 @@ public class Telephony extends AbstractMonitor {
 		 */
 		public static NetworkType valueOf(int value) {
 			switch (value) {
-				case TelephonyManager.NETWORK_TYPE_UMTS:
-					return UMTS;
-				case TelephonyManager.NETWORK_TYPE_HSDPA:
-					return HSDPA;
-				case TelephonyManager.NETWORK_TYPE_EDGE:
-					return EDGE;
+				case TelephonyManager.NETWORK_TYPE_1xRTT:
+					return RTT;
 				case TelephonyManager.NETWORK_TYPE_CDMA:
 					return CDMA;
+				case TelephonyManager.NETWORK_TYPE_EDGE:
+					return EDGE;
+				case TelephonyManager.NETWORK_TYPE_EHRPD:
+					return EHRPD;
+				case TelephonyManager.NETWORK_TYPE_EVDO_0:
+					return EVDO_0;
+				case TelephonyManager.NETWORK_TYPE_EVDO_A:
+					return EVDO_A;
+				case TelephonyManager.NETWORK_TYPE_EVDO_B:
+					return EVDO_B;
 				case TelephonyManager.NETWORK_TYPE_GPRS:
 					return GPRS;
-				case TelephonyManager.NETWORK_TYPE_LTE:
-					return LTE;
+				case TelephonyManager.NETWORK_TYPE_HSDPA:
+					return HSDPA;
 				case TelephonyManager.NETWORK_TYPE_HSPA:
 					return HSPA;
 				case TelephonyManager.NETWORK_TYPE_HSPAP:
 					return HSPAP;
+				case TelephonyManager.NETWORK_TYPE_HSUPA:
+					return HSUPA;
+				case TelephonyManager.NETWORK_TYPE_IDEN:
+					return IDEN;
+				case TelephonyManager.NETWORK_TYPE_LTE:
+					return LTE;
+				case TelephonyManager.NETWORK_TYPE_UMTS:
+					return UMTS;
+				case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+					return UNKNOWN;
 			}
 			return OTHER;
 		}
@@ -155,13 +187,20 @@ public class Telephony extends AbstractMonitor {
 				GsmCellLocation loc = (GsmCellLocation) location;
 
 				data.put(TelephonyData.TIMESTAMP, System.currentTimeMillis());
-				data.put(TelephonyData.TELEPHONY_STANDARD, "gsm");
+				data.put(TelephonyData.TELEPHONY_STANDARD, TelephonyStandard.GSM.getValue());
 				data.put(TelephonyData.TELEPHONY_GSM_CID, loc.getCid());
 				data.put(TelephonyData.TELEPHONY_GSM_LAC, loc.getLac());
-				data.put(TelephonyData.TELEPHONY_OPERATOR_NAME, telephonyManager.getNetworkOperatorName());
+				
+				/* TODO: This will probably fail if there is no network */
+				String operator = telephonyManager.getNetworkOperator();
+				int mcc = Integer.valueOf(operator.substring(0,3));
+				int mnc = Integer.valueOf(operator.substring(3));
+				
+				data.put(TelephonyData.TELEPHONY_OPERATOR_MCC, mcc);
+				data.put(TelephonyData.TELEPHONY_OPERATOR_MNC, mnc);
 				
 				if (lastSignalStrength != null) {
-					synchronized(lastSignalStrength) {
+					synchronized(syncSignalStrength) {
 						/* convert the Signal Strength from GSM to Dbm */
 						if (lastSignalStrength.getGsmSignalStrength() != 99) {
 							float signalStrengthDbm = (lastSignalStrength
@@ -170,7 +209,7 @@ public class Telephony extends AbstractMonitor {
 									signalStrengthDbm);
 						}
 						if (lastSignalStrength.getGsmBitErrorRate() != 99) {
-							double gsmBerPercent = gsmBerTable[lastSignalStrength.getGsmBitErrorRate()] / 100.0;
+							double gsmBerPercent = gsmBerTable[lastSignalStrength.getGsmBitErrorRate()] / 100;
 							data.put(TelephonyData.TELEPHONY_SIGNAL_BER,
 									gsmBerPercent);
 						}
@@ -178,34 +217,37 @@ public class Telephony extends AbstractMonitor {
 				}
 				
 				if (lastNetworkType != null){
-					synchronized (lastNetworkType) {
-						data.put(TelephonyData.TELEPHONY_NETWORK_TYPE, lastNetworkType.getValue());
-					}
+					data.put(TelephonyData.TELEPHONY_NETWORK_TYPE, lastNetworkType.getValue());
 					
 				}
 				
 				data.put(TelephonyData.TELEPHONY_GSM_PSC, loc.getPsc());
 
-				/* TODO add neighbor list? */
+				// Add list of neighbors
 				List<NeighboringCellInfo> neighbors = telephonyManager.getNeighboringCellInfo();
 				if( neighbors.size() > 0 ) {
 					List<DataObject> dataNeighborlist = new ArrayList<DataObject>();
-                    for(NeighboringCellInfo neighbor : neighbors ) {
-                    	
-                    	DataObject dataNeighbor = new ContentValuesDataObject();
-                    	// TODO: This does not work, it has to go inside a list
-                    	dataNeighbor.put(TelephonyData.TIMESTAMP, System.currentTimeMillis());
-                    	dataNeighbor.put(TelephonyData.TELEPHONY_NEIGHBOR_GSM_CID, neighbor.getCid());
-                    	dataNeighbor.put(TelephonyData.TELEPHONY_NEIGHBOR_GSM_LAC, neighbor.getLac());
-                    	dataNeighbor.put(TelephonyData.TELEPHONY_NEIGHBOR_GMS_PSC, neighbor.getPsc());
+                    for(NeighboringCellInfo neighbor : neighbors) {
+                    	DataObject neighborData = new ContentValuesDataObject();
+                    	neighborData.put(TelephonyData.TELEPHONY_GSM_CID, neighbor.getCid());
+                    	neighborData.put(TelephonyData.TELEPHONY_GSM_LAC, neighbor.getLac());
+                    	neighborData.put(TelephonyData.TELEPHONY_GSM_PSC, neighbor.getPsc());
                         
                         if(neighbor.getRssi() != NeighboringCellInfo.UNKNOWN_RSSI){
-                        		float signalNeighborStrength = (neighbor.getRssi()*2) - 113;
-                        		dataNeighbor.put(TelephonyData.TELEPHONY_NEIGHBOR_SIGNAL_STRENGTH,signalNeighborStrength);
+                        		float neighborSignalStrength = (neighbor.getRssi()*2) - 113;
+                        		neighborData.put(TelephonyData.TELEPHONY_SIGNAL_STRENGTH, neighborSignalStrength);
                         }
-                        dataNeighborlist.add(dataNeighbor);
+                        
+                        // Only add to the list if we have neighbor info
+                        if (neighbor.getCid() != NeighboringCellInfo.UNKNOWN_CID
+								&& neighbor.getLac() != NeighboringCellInfo.UNKNOWN_CID) {
+                        	dataNeighborlist.add(neighborData);
+                    	}
                     }
-                    data.put(TelephonyData.TELEPHONY_NEIGHBOR_LIST,dataNeighborlist);
+                    
+                    if (dataNeighborlist.size() > 0) {
+                    	data.put(TelephonyData.TELEPHONY_NEIGHBOR_LIST, dataNeighborlist);
+                    }
                 }
 
 			} else {
@@ -213,8 +255,7 @@ public class Telephony extends AbstractMonitor {
 				
 				data.put(TelephonyData.TIMESTAMP, 
 						System.currentTimeMillis());
-				data.put(TelephonyData.TELEPHONY_STANDARD,
-						"cdma");
+				data.put(TelephonyData.TELEPHONY_STANDARD, TelephonyStandard.CDMA.getValue());
 				data.put(TelephonyData.CDMA_BASE_STATION,
 						loc.getBaseStationId());
 				data.put(TelephonyData.CDMA_BASE_LONGITUDE,
@@ -223,12 +264,19 @@ public class Telephony extends AbstractMonitor {
 						loc.getBaseStationLatitude());
 				data.put(TelephonyData.CDMA_NETWORK_ID, 
 						loc.getNetworkId());
-				data.put(TelephonyData.TELEPHONY_OPERATOR_NAME, 
-						telephonyManager.getNetworkOperatorName());
 				
+				/* TODO: This will probably fail if there is no network */
+				String operator = telephonyManager.getSimOperator();
+				int mcc = Integer.valueOf(operator.substring(0,3));
+				int mnc = Integer.valueOf(operator.substring(3));
+				
+				data.put(TelephonyData.TELEPHONY_OPERATOR_MCC, 
+						mcc);
+				data.put(TelephonyData.TELEPHONY_OPERATOR_MNC, 
+						mnc);
 				
 				if (lastSignalStrength != null) {
-					synchronized(lastSignalStrength) {
+					synchronized(syncSignalStrength) {
 						data.put(TelephonyData.TELEPHONY_SIGNAL_STRENGTH,
 								lastSignalStrength.getCdmaDbm());
 						data.put(TelephonyData.EVDO_DBM,
@@ -241,10 +289,7 @@ public class Telephony extends AbstractMonitor {
 				}
 				
 				if (lastNetworkType != null){
-					synchronized (lastNetworkType) {
-						data.put(TelephonyData.TELEPHONY_NETWORK_TYPE, lastNetworkType.getValue());
-					}
-					
+					data.put(TelephonyData.TELEPHONY_NETWORK_TYPE, lastNetworkType.getValue());
 				}
 			}
 			
@@ -252,21 +297,19 @@ public class Telephony extends AbstractMonitor {
 			notifyListeners(telephonyEvent, new BasicMonitorEventResult(data));
 
 			/* Log the results */
-			Log.d(TAG, data.toString());
+			if (DEBUG) Log.d(TAG, data.toString());
 		}
 
 		@Override
 		public void onDataConnectionStateChanged(int state, int networkType){
     		super.onDataConnectionStateChanged(state, networkType);
-			synchronized (lastNetworkType) {
-				lastNetworkType = NetworkType.valueOf(networkType);
-			}
+			lastNetworkType = NetworkType.valueOf(networkType);
     	}
 		
 		@Override
 		public void onSignalStrengthsChanged(SignalStrength signalStrength) {
 			super.onSignalStrengthsChanged(signalStrength);
-			synchronized (lastSignalStrength) {
+			synchronized(syncSignalStrength) {
 				lastSignalStrength = signalStrength;
 			}
 			/* TODO: add signal now to DB? */
@@ -274,6 +317,7 @@ public class Telephony extends AbstractMonitor {
 	}
 
 	private SignalStrength lastSignalStrength = null;
+	private Object syncSignalStrength = new Object();
 	private NetworkType lastNetworkType = null;
 
 	/**
@@ -282,6 +326,8 @@ public class Telephony extends AbstractMonitor {
 	private final IBinder serviceBinder = new ServiceBinder();
 	
 	protected String TAG = "AdkintunMobile::Telephony";
+	
+	private Context mContext = this;
 
 	private MonitorEvent telephonyEvent = new AbstractMonitorEvent() {
 		@Override
@@ -292,9 +338,15 @@ public class Telephony extends AbstractMonitor {
 						PhoneStateListener.LISTEN_CELL_LOCATION	| PhoneStateListener.LISTEN_SIGNAL_STRENGTHS 
 						| PhoneStateListener.LISTEN_DATA_CONNECTION_STATE);
 				
-				super.activate();
+				/* Get the initial network type */
+				ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+				NetworkInfo ni = connectivityManager.getActiveNetworkInfo();
+				if (ni != null) {
+					lastNetworkType = NetworkType.valueOf(ni.getType());
+				}
 				
-				Log.d(TAG, "Telephony service has been activated");
+				if (DEBUG) Log.d(TAG, "Telephony service has been activated");
+				super.activate();
 			}
 			return true;
 		}
@@ -304,9 +356,9 @@ public class Telephony extends AbstractMonitor {
 			if (isActive()) {
 				telephonyManager.listen(telephonyStateListener,
 						PhoneStateListener.LISTEN_NONE);
-				super.deactivate();
 				
-				Log.d(TAG, "Telephony service has been deactivated");
+				if (DEBUG) Log.d(TAG, "Telephony service has been deactivated");
+				super.deactivate();
 			}
 		}
 		
@@ -324,7 +376,7 @@ public class Telephony extends AbstractMonitor {
 
 	@Override
 	public boolean activate(int events, Bundle configuration) {
-		if ((events & SCREEN) == SCREEN) {
+		if ((events & TELEPHONY) == TELEPHONY) {
 			return activate(telephonyEvent);
 		}
 		return false;
