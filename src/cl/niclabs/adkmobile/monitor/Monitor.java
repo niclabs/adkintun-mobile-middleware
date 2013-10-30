79f66bfc7272d6c7f0fc6d6bb3c4eafe97cf801c
Mauricio Castro <mauricio@niclabs.cl>
2013-10-30 15:37:17 -0400

    Added the PhoneStatus service. AutoStart is inside the PhoneStatus class. TODO  define what the AutoStart should do. Finished the SIM Status methods (on Telephony Class).

--- src/cl/niclabs/adkmobile/monitor/Monitor.java
+++ src/cl/niclabs/adkmobile/monitor/Monitor.java
@@ -69,6 +69,11 @@ public interface Monitor<E extends MonitorListener> {
 	public static final int SCREEN = 16;
 	
 	/**
+	 * Dfines a phone status event.
+	 */
+	public static final int PHONESTATUS = 32;
+	
+	/**
 	 * Activate the specified events.
 	 * @param events
 	 * @param configuration additional configurations for the events
