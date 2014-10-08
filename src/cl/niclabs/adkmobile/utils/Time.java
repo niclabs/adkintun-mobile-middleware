d086b457ff0380d99a6c4ade21f75da3b54ccf79
Felipe Lalanne <flalanne@niclabs.cl>
2014-10-07 22:37:09 -0300

    Create currentTime on cl.niclabs.adkmobile.utils.Time

--- src/cl/niclabs/adkmobile/utils/Time.java
+++ src/cl/niclabs/adkmobile/utils/Time.java
@@ -1,9 +1,23 @@
 package cl.niclabs.adkmobile.utils;
 
+import java.util.Date;
+
 import cl.niclabs.adkmobile.services.ClockService;
 
 public class Time {
+	/**
+	 * Get current time in milliseconds since Jan. 1, 1970 GMT.
+	 * @return
+	 */
 	public static long currentTimeMillis() {
 		return ClockService.currentTimeMillis();
+	}
+	
+	/**
+	 * Get current time in UTC as a date object
+	 * @return
+	 */
+	public static Date currentTime() {
+		return new Date(ClockService.currentTimeMillis());
 	}
 }
