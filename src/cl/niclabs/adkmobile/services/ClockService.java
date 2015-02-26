650be7bccf4afc78a266664222b000942960c193
Felipe Lalanne <flalanne@niclabs.cl>
2015-02-26 17:13:37 -0300

    Added ClockService stub for backwards compatibility

---
+++ src/cl/niclabs/adkmobile/services/ClockService.java
@@ -0,0 +1,13 @@
+package cl.niclabs.adkmobile.services;
+
+import cl.niclabs.adkmobile.utils.Clock;
+
+/**
+ * Class left for backwards compatibility. All functionality
+ * has been distributed between cl.niclabs.adkmobile.utils.Clock and 
+ * cl.niclabs.adkmobile.monitor.Clock
+ *  
+ * @author Felipe Lalanne <flalanne@niclabs.cl>
+ * @deprecated cl.niclabs.utils.Clock must be used instead
+ */
+public class ClockService extends Clock {}
