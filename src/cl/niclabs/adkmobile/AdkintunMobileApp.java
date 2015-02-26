2a7a7ffa07b9b140178921dd7007d290ed244153
Felipe Lalanne <flalanne@niclabs.cl>
2015-02-26 18:16:22 -0300

    Moved AdkintunMobileApp to NicLabsApp for all NIC Labs projects

--- src/cl/niclabs/adkmobile/AdkintunMobileApp.java
+++ src/cl/niclabs/adkmobile/AdkintunMobileApp.java
@@ -1,14 +1,8 @@
 package cl.niclabs.adkmobile;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import cl.niclabs.adkmobile.monitor.ClockSynchronization;
 import cl.niclabs.adkmobile.monitor.Monitor;
-import cl.niclabs.adkmobile.utils.Scheduler;
-
-import com.orm.SugarApp;
+import cl.niclabs.android.NicLabsApp;
 
 /**
  * Base class for Adkintun Mobile Applications. 
@@ -28,97 +22,19 @@ import com.orm.SugarApp;
  * 
  * @author Felipe Lalanne <flalanne@niclabs.cl>
  */
-public class AdkintunMobileApp extends SugarApp {
-	/**
-	 * Debugging status of the application
-	 */
-	public static boolean DEBUG = true;
-	
+public class AdkintunMobileApp extends NicLabsApp {	
 	/**
 	 * Library version
 	 */
-	public static final String VERSION = "1.0b";
-	
-	/**
-	 * Define persistence state for the whole application
-	 */
-	public static boolean PERSISTENCE_ENABLED = true;
-	
-	/**
-	 * Library settings file
-	 */
-	public static final String LIBRARY_SETTINGS = "cl.niclabs.adkmobile.settings"; 
-	
-	/**
-	 * Application context, will be null if the application is not added to the manifest
-	 */
-	private static AdkintunMobileApp adkintunMobileContext;
-
-	/**
-	 * Get application context (null if the application is not added in manifest)
-	 * @return
-	 */
-	public static Context getContext() {
-		return adkintunMobileContext;
-	}
-	
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-		
-		// Shutdown the scheduler
-		Scheduler.getInstance().shutdown();
-	}
-	
-	/**
-	 * Get PERSISTENT key value from manifest metadata if available 
-	 * @return
-	 */
-	private boolean isPersistenceEnabled() {
-		try {
-			ApplicationInfo ai = getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
-			return ai.metaData.getBoolean("PERSISTENT", PERSISTENCE_ENABLED);
-		} catch (NameNotFoundException e) {
-		}
-		return PERSISTENCE_ENABLED;
-	}
-	
-	/**
-	 * Get ADK_DEBUG ley from manifest metadata if available, otherwise use default
-	 * value in variable DEBUG
-	 * @return
-	 */
-	private boolean inDebugEnabled() {
-		try {
-			ApplicationInfo ai = getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
-			return ai.metaData.getBoolean("ADK_DEBUG", DEBUG);
-		} catch (NameNotFoundException e) {
-		}
-		return DEBUG;
-	}
+	public static final String VERSION = "1.3.1b";
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		
-		PERSISTENCE_ENABLED = isPersistenceEnabled();
-		DEBUG = inDebugEnabled();
-		
 		// Start the ClockSynchronization monitor
 		ClockSynchronization
 			.bind(ClockSynchronization.class, this)
 			.activate(Monitor.CLOCK);
-		
-		/* Set context (it will be null if application is not added to manifest_ */
-		adkintunMobileContext = this;
-	}
-
-
-	/**
-	 * 
-	 * @return true if persistence is available for this application
-	 */
-	public static boolean isPersistenceAvailable() {
-		return adkintunMobileContext != null && PERSISTENCE_ENABLED;
 	}
 }
