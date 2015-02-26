61116b2f3d67486e3b0edb33ead6d7a62d6ddeb9
Felipe Lalanne <flalanne@niclabs.cl>
2015-02-26 18:13:44 -0300

    Created stub cl.niclabs.adkmobile.DeviceInfo for back compatibility

---
+++ src/cl/niclabs/adkmobile/DeviceInfo.java
@@ -0,0 +1,15 @@
+package cl.niclabs.adkmobile;
+
+import android.content.Context;
+
+/**
+ * Stub class for backwards compatibility
+ * 
+ * @author Felipe Lalanne <flalanne@niclabs.cl>
+ * @deprecated Use cl.niclabs.android.DeviceInfo
+ */
+public class DeviceInfo extends cl.niclabs.android.DeviceInfo {
+	private DeviceInfo(Context context) {
+		super(context);
+	}
+}
