26d7d3ab7d3c38f3d0de88bcd7aa41a96ddda8e4
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-05 13:05:10 -0300

    Updated version to 1.3.2b

--- src/cl/niclabs/adkmobile/AdkintunMobileApp.java
+++ src/cl/niclabs/adkmobile/AdkintunMobileApp.java
@@ -26,7 +26,7 @@ public class AdkintunMobileApp extends NicLabsApp {
 	/**
 	 * Library version
 	 */
-	public static final String VERSION = "1.3.1b";
+	public static final String VERSION = "1.3.2b";
 	
 	@Override
 	public void onCreate() {
